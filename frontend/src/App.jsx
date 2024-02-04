7c8a9f5f1d27a9fd4b807f45ea8e33cfe91d6532
Volmol16 <109240899+Volmol16@users.noreply.github.com>
2024-02-04 21:14:04 +0300

    frontend
    Basic frontend version-

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <div className='bg-black/90 h-screen'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
